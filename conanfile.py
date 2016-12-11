13dbbe0dc01e3033cf3a190ec15cc574803e20ef
Manu343726 <Manu343726@gmail.com>
2016-12-11 01:13:31 +0100

    Test libexecstream package by running tests as a dependent project

--- conanfile.py
+++ conanfile.py
@@ -10,19 +10,32 @@ class LibExecStream(ConanFile):
     options = { "shared": [True, False] }
     default_options = "shared=True"
     generators = "cmake"
-    exports = "include*", "src*", "CMakeLists.txt", "README.md"
+    exports = "include*", "src*", "test*", "CMakeLists.txt", "README.md"
 
     def build(self):
 	cmake = CMake(self.settings)
 
-        shared_lib = '-DLIBEXECSTREAM_SHARED=' + 'TRUE' if self.options.shared else 'FALSE'
+        shared_lib = '-DLIBEXECSTREAM_SHARED=' + ('TRUE' if self.options.shared else 'FALSE')
 
 	self.run('pwd && cmake %s %s %s' % (self.conanfile_directory, cmake.command_line, shared_lib))
         self.run("cmake --build . %s" % cmake.build_config)
 
     def package(self):
-        self.copy("*.h", keep_path=True)
+        include = os.path.join('include', 'libexecstream')
+        posix = os.path.join(include, 'posix')
+        win = os.path.join(include, 'win')
+        dirs = [include, posix, win]
+
+        for dir in dirs:
+            self.copy("*.h", src=dir, dst=dir, keep_path=False)
+
         self.copy("*.lib", dst="lib", keep_path=False)
         self.copy("*.a", dst="lib", keep_path=False)
         self.copy("*.so", dst="lib", keep_path=False)
         self.copy("*.dll", dst="lib", keep_path=False)
+
+    def package_info(self):
+        self.cpp_info.libs = ['libexecstream']
+
+        if self.settings.os == 'Linux':
+            self.cpp_info.libs.append('pthread')
